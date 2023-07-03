3f64bd3d321cb34ef1e318dd1eea973412ead221
Luka <luka.kato@tunts.com>
2023-07-03 10:52:25 -0300

    22. Buscar cidades de um estado - Inserir cache no Nest.js

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entity/city.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([CityEntity]),
+    CacheModule.register({ ttl: 900000 * 1000 }),
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+})
 export class CityModule {}
